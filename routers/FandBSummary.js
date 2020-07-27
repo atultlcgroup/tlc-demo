b3c9ad01a1a08cabb6cc6d1d3a5255b25bcd7820
Atul Kumar Srivastava <atul.srivastava@tlcgroup.com>
2020-07-27 15:48:40 +0530

    done some changes

--- routers/FandBSummary.js
+++ routers/FandBSummary.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const Routers = express.Router();
 const summaryControllers = require("../controllers/FandBSummary")
-
 Routers.post("/report",summaryControllers.FandBSummaryReport);
-
-
-
 module.exports= Routers;
