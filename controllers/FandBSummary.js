833f158be10466fb8dee06b0dceaacf505fd240b
Atul Kumar Srivastava <atul.srivastava@tlcgroup.com>
2020-07-28 15:12:50 +0530

    done some changes

--- controllers/FandBSummary.js
+++ controllers/FandBSummary.js
@@ -3,7 +3,7 @@ let FandBSummaryReport=(req,res)=>{
     try{
         console.log(`FandBSummaryReport api called in controller`)
         summaryModel.FandBSummaryReport().then(data=>{
-            res.status(200).send({code: 200, message: 'success' , data : data})
+            res.status(200).send({code: 200, message: 'success' })
         }).catch(e=>{
             res.status(500).send({code: 500, message: e})
         res.status(200).json({code : 200 , message : 'success'})
