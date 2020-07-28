1287e83af6bfae476bd9aa441485ea4f7e855f63
Atul Kumar Srivastava <atul.srivastava@tlcgroup.com>
2020-07-28 15:00:49 +0530

    done some changes

--- models/FandBSummary.js
+++ models/FandBSummary.js
@@ -7,14 +7,19 @@ console.log(BREAKFASTTIME, LUNCHTIME,DINNERTIME)
 let findNetSpentAPCTCovers = (outlet_id)=>{
     return new Promise(async(resolve,reject)=>{
         try{
-           
           let data = await pool.query(`Select SUM (cast(pos_cheque_details__c.net_amount__c AS DOUBLE PRECISION)) AS totalamount,
           SUM (cast(pos_cheque_details__c.covers__c AS DOUBLE PRECISION)) AS totalcovers,
-          SUM( cast(pos_cheque_details__c.net_amount__c AS DOUBLE PRECISION))/SUM (cast(pos_cheque_details__c.covers__c AS DOUBLE PRECISION)) AS APC
-          from tlcsalesforce.pos_cheque_details__c where  date_part('year', actual_bill_date__c)= date_part('year',CURRENT_DATE) 
+          SUM( cast(pos_cheque_details__c.net_amount__c AS DOUBLE PRECISION))/SUM (cast(pos_cheque_details__c.covers__c AS DOUBLE PRECISION)) AS apc
+          from tlcsalesforce.pos_cheque_details__c where length(net_amount__c) > 0 and length(covers__c) > 0 and date_part('year', actual_bill_date__c)= date_part('year',CURRENT_DATE) 
           and date_part('month', actual_bill_date__c) = date_part('month',CURRENT_DATE) AND actual_bill_date__c<=CURRENT_DATE AND outlet__c='${outlet_id}'`)
-        const result = data ? data.rows : [{totalamount: 0,totalcovers:0,APC:0}]
-           resolve(result)
+        const result = data ? data.rows : [{totalamount: 0,totalcovers:0,apc:0}]
+        let dataObj={};
+        dataObj['totalamount'] = result[0].totalamount || 0;
+        dataObj['totalcovers'] = result[0].totalcovers || 0;
+        dataObj['apc'] = result[0].apc || 0;
+
+
+           resolve(dataObj)
          }catch( e ){
             reject(e);
         }
@@ -75,12 +80,27 @@ let coversDayWise= (outlet_id)=>{
     })
 }
 
-let insertSummarizedDataToFandB=async()=>{
+let insertSummarizedDataToFandB=async(netSpentAPCTCovers,coversInDBL,coversOfDayInCurrentMonth,outlet_id)=>{
     return new Promise(async(resolve,reject)=>{
-        let data = await pool.query(`INSERT INTO tlcsalesforce.f_b_summary__c(
-            covers_for_wed__c, name, net_spends__c, covers_by_time_in_a_day_for_breakfast__c, covers_by_time_in_a_day_for_dinner__c, isdeleted, systemmodstamp, covers_for_mon__c, covers_for_sun__c, apc_month_wise__c, outlet__c, createddate, covers_for_sat__c, covers_for_tue__c, covers_by_time_in_a_day_for_lunch__c, covers_for_thr__c, covers_for_fri__c, sfid, id, _hc_lastop, _hc_err)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`);
-
+        try{
+            let checkOutletForMonth = await pool.query(`select id from tlcsalesforce.f_b_summary__c where  date_part('year', createddate)= date_part('year',CURRENT_DATE) 
+            and date_part('month', createddate) = date_part('month',CURRENT_DATE) and outlet__c='${outlet_id}'`)
+            let data = checkOutletForMonth ? checkOutletForMonth.rows : [];
+            if(data.length ==0){
+            await pool.query(`INSERT INTO tlcsalesforce.f_b_summary__c(
+            covers_for_wed__c, net_spends__c, covers_by_time_in_a_day_for_breakfast__c, covers_by_time_in_a_day_for_dinner__c, covers_for_mon__c, covers_for_sun__c, apc_month_wise__c, outlet__c, createddate, covers_for_sat__c, covers_for_tue__c, covers_by_time_in_a_day_for_lunch__c, covers_for_thr__c, covers_for_fri__c)
+            VALUES ('${coversOfDayInCurrentMonth['Wednesday']}','${netSpentAPCTCovers['totalamount']}','${coversInDBL['covers_in_breakfaset']}','${coversInDBL['covers_in_dinner']}','${coversOfDayInCurrentMonth['Monday']}','${coversOfDayInCurrentMonth['Sunday']}','${netSpentAPCTCovers['apc']}','${outlet_id}',NOW(),'${coversOfDayInCurrentMonth['Saturday']}','${coversOfDayInCurrentMonth['Tuesday']}','${coversInDBL['covers_in_lunch']}','${coversOfDayInCurrentMonth['Thursday']}','${coversOfDayInCurrentMonth['Friday']}')`);
+            console.log(`Data Inserted Successfully in f_b_summary__c !`)
+            }else{
+            await pool.query(`update tlcsalesforce.f_b_summary__c set 
+            covers_for_wed__c= '${coversOfDayInCurrentMonth['Wednesday']}', net_spends__c='${netSpentAPCTCovers['totalamount']}', covers_by_time_in_a_day_for_breakfast__c ='${coversInDBL['covers_in_breakfaset']}', covers_by_time_in_a_day_for_dinner__c = '${coversInDBL['covers_in_dinner']}', covers_for_mon__c = '${coversOfDayInCurrentMonth['Monday']}', covers_for_sun__c ='${coversOfDayInCurrentMonth['Sunday']}', apc_month_wise__c = '${netSpentAPCTCovers['apc']}', outlet__c= '${outlet_id}', covers_for_sat__c = '${coversOfDayInCurrentMonth['Saturday']}', covers_for_tue__c = '${coversOfDayInCurrentMonth['Tuesday']}', covers_by_time_in_a_day_for_lunch__c ='${coversInDBL['covers_in_lunch']}', covers_for_thr__c = '${coversOfDayInCurrentMonth['Thursday']}', covers_for_fri__c='${coversOfDayInCurrentMonth['Friday']}' where date_part('year', createddate)= date_part('year',CURRENT_DATE) 
+            and date_part('month', createddate) = date_part('month',CURRENT_DATE) and outlet__c='${outlet_id}'`);
+            console.log(`Data Updated Successfully in f_b_summary__c !`)
+            }
+            resolve(`Success`)
+        }catch(e){
+            reject(`${e}`)
+        }
     })
 
 }
@@ -97,6 +117,7 @@ let FandBSummaryReport = async () => {
                     const netSpentAPCTCovers = await findNetSpentAPCTCovers(d.outlet_id)
                     const coversInDBL = await totalCoversInDBL(d.outlet_id)
                     const coversOfDayInCurrentMonth= await coversDayWise(d.outlet_id)
+                    await insertSummarizedDataToFandB(netSpentAPCTCovers,coversInDBL,coversOfDayInCurrentMonth,d.outlet_id)
                 }
             resolve(data)
         } catch (e) {
