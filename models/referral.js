c8a78bc2e56ec272373329f17df3da9113e0a70d
atul511996 <atul.srivastava@tlcgroup.com>
2020-09-07 12:15:22 +0530

    done some changes

--- models/referral.js
+++ models/referral.js
@@ -36,10 +36,14 @@ let offerApplicableFor=async(member_type, referral_code, membership_number)=>{
 
 let getRefferalData2=  (data,header)=>{
     return new Promise(async(resolve,reject)=>{
+        console.log('Method called')
         try{     
-           let result= await pool.query(`select referral_code__c,member_id__c,membership_number__C from tlcsalesforce.account p1 inner join tlcsalesforce.membership__c p2 on p1.member_id__c = p2.member__r__member_id__C where p1.referral_code__c='${data.referralCode}'`)
+            console.log('request body referral api::', data)  
+           let result= await pool.query(`select referral_code__c,member_id__c,membership_number__C from tlcsalesforce.account p1 inner join tlcsalesforce.membership__c p2 on p1.member_id__c = p2.member__r__member_id__c where p1.referral_code__c='${data.referralCode}'`)
+           
            let validateResult=result ? result.rows : [];
              if(validateResult.length == 0){
+                 console.log('data::', data.referralCode)
                  reject(`Referral code not valid!`)
              }
         let finalResultOfJoinBenefitAndProgarm=await fetchReferralProgramAndRefferalBenefits(data,header)
@@ -52,13 +56,14 @@ let getRefferalData2=  (data,header)=>{
             console.log(`++++++ MembershipNumber : ${memberShipNumber}++++++`)
             let offerData =await createOffers(d.benefit__c, memberShipNumber  )
             console.log(`++++++ offerData : ${offerData}++++++`)
+            d.agent = d.agent ? d.agent : null
             let pushIntoRefferlConersion=await pool.query(`INSERT INTO tlcsalesforce.referral_conversion__c(
-                counter_party__c, rule_used__c, member_type__c, referred_date__c, member__c, name, benefit__c,  createddate, agent__c)
+                counter_party__c, rule_used__c, member_type__c, referred_date__c, member__r__member_id__c, name, benefit__c,  createddate, agent__c)
                 VALUES ('${validateResult[0].member_id__c}','${d.sfid2}', '${d.member_type__c}', now(),'${header.memberid}', '${d.transaction_type__c}', '${d.sfid2}', now(), '${data.agent}')`);
         }
            resolve('Offer Attached Successfylly!!')
         }catch(err){
-            console.log("under the promise reject");
+            console.log("under the promise reject", err.message);
             reject("promise reject");
         }
     })
@@ -66,10 +71,11 @@ let getRefferalData2=  (data,header)=>{
 
 let fetchReferralProgramAndRefferalBenefits=async(data,header)=>{
     try{
-    let resultOfJoinBenefitAndProgarm=await pool.query(`select p1.name,transaction_type__c,end_date__c,start_date__c,program__r__unique_identifier__c,p1.sfid sfid1,p2.isdeleted,benefit__c,member_type__c,p2.sfid sfid2 from tlcsalesforce.referral_program__c p1 inner join tlcsalesforce.referral_benefits__C p2 on p1.sfid=p2.referral_program__c where p1.transaction_type__c='${data.transactionType}' and program__r__unique_identifier__c='${header.programid}' and p1.start_date__c::date <= NOW()::date and end_date__c >= NOW()::date`);
+    let resultOfJoinBenefitAndProgarm=await pool.query(`select p1.name,transaction_type__c,end_date__c,start_date__c,program__r__unique_identifier__c,p1.sfid sfid1,p2.isdeleted,benefit__c,member_type__c,p2.sfid sfid2 from tlcsalesforce.referral_program__c p1 inner join tlcsalesforce.referral_benefits__C p2 on p1.sfid=p2.referral_program__c where p1.transaction_type__c='${data.transactionType}' and program__r__unique_identifier__c='${header.program_id}' and p1.start_date__c::date <= NOW()::date and end_date__c >= NOW()::date`);
     let finalResultOfJoinBenefitAndProgarm=resultOfJoinBenefitAndProgarm ? resultOfJoinBenefitAndProgarm.rows : [];
     return finalResultOfJoinBenefitAndProgarm
     }catch(e){
+        console.log('exception::', e.message)
         return e
     }
 }
--- routers/referral.js
+++ routers/referral.js
@@ -2,5 +2,5 @@
 const express = require("express");
 const Routers = express.Router();
 const excelControllers = require("../controllers/referral")
-Routers.post("/getRefferalData",excelControllers.getRefferalData2);
+Routers.post("/attachReferralOffer",excelControllers.getRefferalData2);
 module.exports= Routers;
