23e7ed15967064761ba63c858b8b154a199ec8d9
atul511996 <atul.srivastava@tlcgroup.com>
2020-10-06 14:10:02 +0530

    done changes

--- controllers/paymentLink.js
+++ controllers/paymentLink.js
@@ -1,13 +1,15 @@
 let getPaymentLink = async(req, res)=>{
     try{
     console.log(`uploadExcel api called in controller`)
-    if(!req.body.uid){
+    if(!req.query.uid){
         res.status(401).send({code: 401, message: 'Please provide UId'})
         return
     }
-  
+    let str = req.url
+      let uid = str.substr(str.indexOf('uid=') + 4)
+      console.log(uid)
         let paymentUrl = process.env.PAYMENT_LINK || ""
-        res.status(200).send({code:200 , message : `Success`,paymentURL:  `${paymentUrl}${req.body.uid}`})
+        res.status(200).send({code:200 , message : `Success`,paymentURL:  `${paymentUrl}${uid}`})
         
  
    }catch( e ){
