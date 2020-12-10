578d9502b67c029c4cf6d484ec4bcc215b07807b
atul511996 <atul.srivastava@tlcgroup.com>
2020-12-10 17:39:52 +0530

    changes done

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ const app = express();
 app.use(cors());
 app.use(body_parser.urlencoded({limit: "50mb", extended: false}))
 app.use(body_parser.json({limit: '50mb'}));
-
 app.use(helmet())
 app.use("/api",posRouters)
 app.use("/api/pos",excelRouters)
