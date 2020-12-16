916ec2197585a82cd4f03a5decfa246b5eca8846
atul511996 <atul.srivastava@tlcgroup.com>
2020-12-16 12:10:42 +0530

    done changes for worker dynos

--- check.js
+++ check.js
@@ -1,4 +1,6 @@
 const schedule = require('node-schedule');
+console.log(`=================  From worker dyno  ========================`)
+
 let scheduledTime = `* * * * *`
  schedule.scheduleJob(scheduledTime, async()=>{
     console.log(`=================  From worker dyno  ========================`)
