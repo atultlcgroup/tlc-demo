31b26f187aad2af6b7f63f84244a487b4a93b2aa
atul511996 <atul.srivastava@tlcgroup.com>
2020-12-18 16:25:18 +0530

    done changes for worker

--- check.js
+++ check.js
@@ -2,8 +2,8 @@ const schedule = require('node-schedule');
 console.log(`=================  From worker dyno  ========================`)
 
 const pool = require("./databases/db").pool
-let scheduledTime = `* * * * *`;
- schedule.scheduleJob(scheduledTime, async()=>{
-    console.log(`=================  From worker dyno  ========================`)
-    console.log(`=================   From worer dyno   =======================`)
-  });
+// let scheduledTime = `* * * * *`;
+//  schedule.scheduleJob(scheduledTime, async()=>{
+//     console.log(`=================  From worker dyno  ========================`)
+//     console.log(`=================   From worer dyno   =======================`)
+//   });
