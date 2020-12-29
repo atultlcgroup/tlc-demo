a6baabbaf17b13a8552ccb1928b58a8eb95cbc1a
atul511996 <atul.srivastava@tlcgroup.com>
2020-12-29 12:54:40 +0530

    done changes

--- check.js
+++ check.js
@@ -1,8 +1,5 @@
-const schedule = require('node-schedule');
-console.log(`=================  From worker dyno  ========================`)
-
-let scheduledTime = `* * * * *`
- schedule.scheduleJob(scheduledTime, async()=>{
-    console.log(`=================  From worker dyno  ========================`)
-    console.log(`=================   From worer dyno   ========================`)
-  });
+let dateTime = `${String((new Date('2020-12-21')).getDate())}`
+console.log(dateTime)
+let daily = true;
+dateTime == '1' ? daily = false : ''
+console.log(daily)
