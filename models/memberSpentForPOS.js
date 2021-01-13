c0f2d17c228d5523c88ccd550bb663730d312cd0
atul511996 <atul.srivastava@tlcgroup.com>
2021-01-13 11:03:58 +0530

    done all chnages

--- models/memberSpentForPOS.js
+++ models/memberSpentForPOS.js
@@ -62,6 +62,7 @@ let getMembershipDetails = async()=>{
                }
                let memberShipArrForM = await getMemnershipNumberForPOSCDM()
                if(memberShipArrForM.length ){
+                console.log(memberShipArrForM.length)
                 let getMembershipDataForMonth=await pool.query(`select membership_status__C,expiry_date__c,member__c,membership_number__c,sfid from tlcsalesforce.membership__c where membership_number__c is not NULL and membership_number__c IN (${memberShipArrForM})`)
                 resultMonth= getMembershipDataForMonth ? getMembershipDataForMonth.rows : []; 
                }else{
@@ -108,8 +109,10 @@ let getPOSchequeDetailsData=async(membershipDataForDay ,membershipDataForMonth ,
                 }
                 if(!daily){
                     let membershipStatusForMonth;
+                    let times =0;
                     console.log("member spent data from POS");
-                    for(obj1 of membershipDataForMonth){              
+                    for(obj1 of membershipDataForMonth){         
+                        console.log(`times = ${times++} ${obj1.membership_number__c} `)     
                                 let spendDataForMonth=await pool.query(`select sum(case when bill_date__c >= date_trunc('month',(CURRENT_DATE - INTERVAL '6 months')) and bill_date__c <= date_trunc('month',(CURRENT_DATE - INTERVAL '1 months')) + INTERVAL '1 MONTH - 1 day' THEN gross_bill_total__C ELSE 0 END) as last_six_month,
                                 sum(case when EXTRACT(YEAR FROM bill_date__c) = EXTRACT(YEAR FROM CURRENT_DATE) - 1 THEN gross_bill_total__C ELSE 0 END) as last_year,
                                 sum(case when bill_date__c >= date_trunc('month',(CURRENT_DATE - INTERVAL '3 months')) and bill_date__c <= date_trunc('month',(CURRENT_DATE - INTERVAL '1 months')) + INTERVAL '1 MONTH - 1 day' THEN gross_bill_total__C ELSE 0 END) AS last_three_months
