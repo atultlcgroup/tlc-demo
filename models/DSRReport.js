0a36816f33a36a2aaa16064586b952f1dbd7b116
atul511996 <atul.srivastava@tlcgroup.com>
2021-01-19 13:27:08 +0530

    all changes done required for DSR

--- models/DSRReport.js
+++ models/DSRReport.js
@@ -268,7 +268,7 @@ let sfdcApiCall =  async(propertyId, date)=>{
                 if(fileData.code== 200){
                     console.log(`yes! file exists for given fileId`)
                     // let fileName = await saveSfdcFile(fileData.msg, propertyId)
-                    fileArr.push({name: fileName, extension: fileExtension , url : fileData.msg.fileName , sequenceNumber : sequenceNumber })
+                    fileArr.push({name: fileName, extension: fileExtension , url : fileData.msg.fileName , sequenceNumber : `${sequenceNumber} ${convertDateForDOCName()}` })
                     // fileArr.push({name: fileName, extension: fileExtension , url : fileData.msg.fileName , sequenceNumber : sequenceNumber + '1' })
                     // fileArr.push({name: fileName, extension: fileExtension , url : fileData.msg.fileName , sequenceNumber : sequenceNumber + '2' })
 
@@ -288,6 +288,24 @@ let sfdcApiCall =  async(propertyId, date)=>{
     }
 }
 
+
+let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+let convertDateForDOCName= () => {
+  let date1 = new Date()
+  if (date1) {
+      date1.setDate(date1.getDate() - 1); 
+      let today1 = new Date(date1);
+      let hours1 = date1.getHours();
+      let minutes = date1.getMinutes();
+      let ampm = hours1 >= 12 ? 'pm' : 'am';
+      hours1 = hours1 % 12;
+      hours1 = hours1 ? hours1 : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      let strTime = hours1 + ':' + minutes + ' ' + ampm;
+      dateTime = `${String(today1.getDate()).padStart(2, '0')} ${months[String(today1.getMonth() +1)]} ${today1.getFullYear()}`
+  }
+  return dateTime
+}
 
 
 
