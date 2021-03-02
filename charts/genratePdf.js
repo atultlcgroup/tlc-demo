431b7b907e5588bcf5d928fc4449f368a0c41152
atultlcgroup <atul.srivastava@tlcgroup.com>
2021-03-02 18:37:31 +0530

    done chnages

--- charts/genratePdf.js
+++ charts/genratePdf.js
@@ -1,33 +1,45 @@
-    let htmlSTR= `<html>
+    let htmlSTR= `<!DOCTYPE HTML>
+    <html>
     <head>
-      <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-      <script type="text/javascript">
-        google.charts.load("current", {packages:["corechart"]});
-        google.charts.setOnLoadCallback(drawChart);
-        function drawChart() {
-          var data = google.visualization.arrayToDataTable([
-            ['Task', 'Hours per Day'],
-            ['Work',     11],
-            ['Eat',      2],
-            ['Commute',  2],
-            ['Watch TV', 2],
-            ['Sleep',    7]
-          ]);
-  
-          var options = {
-            title: 'My Daily Activities',
-            is3D: true,
-          };
-  
-          var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-          chart.draw(data, options);
-        }
-      </script>
+    <script>
+    window.onload = function() {
+    
+    var chart = new CanvasJS.Chart("chartContainer", {
+      theme: "light2", // "light1", "light2", "dark1", "dark2"
+      // exportEnabled: true,
+      // animationEnabled: true,
+      title: {
+        text: "Desktop Browser Market Share in 2016"
+      },
+      data: [{
+        type: "pie",
+        startAngle: 25,
+        toolTipContent: "<b>{label}</b>: {y}%",
+        showInLegend: "true",
+        legendText: "{label}",
+        indexLabelFontSize: 16,
+        indexLabel: "{label} - {y}%",
+        dataPoints: [
+          { y: 51.08, label: "Chrome" },
+          { y: 27.34, label: "Internet Explorer" },
+          { y: 10.62, label: "Firefox" },
+          { y: 5.02, label: "Microsoft Edge" },
+          { y: 4.07, label: "Safari" },
+          { y: 1.22, label: "Opera" },
+          { y: 0.44, label: "Others" }
+        ]
+      }]
+    });
+    chart.render();
+    
+    }
+    </script>
     </head>
     <body>
-      <div id="piechart_2d" style="width: 900px; height: 500px;"></div>
+    <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
     </body>
-  </html>`
+    </html>`
     const Promise = require('bluebird');
     const pdf = Promise.promisifyAll(require('html-pdf'));
     let data =  pdf.createAsync(htmlSTR, { "height": "10.5in","width": "14.5in", filename: `./charts/abc2.pdf` })
