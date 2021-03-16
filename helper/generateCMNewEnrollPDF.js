a4e64071aa7e8bdfc822cf52131b37ef0f3f29d5
atultlcgroup <atul.srivastava@tlcgroup.com>
2021-03-16 11:43:04 +0530

    done some chnages

--- helper/generateCMNewEnrollPDF.js
+++ helper/generateCMNewEnrollPDF.js
@@ -302,7 +302,7 @@ let htmlStr=`
           </table>
       
     </div>
-    <div class="arilFont" id="pageFooter" style="font-size: 7px; height:500px; bottom:100px;" ><p><b>
+    <div class="arilFont" id="pageFooter" style="font-size: 7px; height:700px; bottom:100px;" ><p><b>
      This is an auto generated report by TLC Relationship Management Private Limited (TLC), (<a href="www.tlcgroup.com">www.tlcgroup.com</a>) and does not require a signature</b></p>
     <p align="left"> MARRIOTT CONFIDENTIAL & PROPRIETARY INFORMATION</p>
     <p>The contents of the document are confidential and proprietary to TLC Relationship Management Private Limited and may not be reproduced, disclosed, distributed or used without the express permission of an authorized representative of TLC Relationship Management Private Limited. Any other use is expressly prohibited.</p>
@@ -316,7 +316,7 @@ console.log("new enrollmet ------")
 let pdfName = `./reports/CMNewEnroll/CM_Enroll_${pId}_${Date.now()}.pdf`
 
 const pdf = Promise.promisifyAll(require('html-pdf'));
-    let data = await pdf.createAsync(`${htmlStr}`, { "height": "10.5in","width": "14.5in", filename: `${pdfName}` })
+    let data = await pdf.createAsync(`${htmlStr}`, { footer: { "height": "30mm"},"height": "10.5in","width": "14.5in", filename: `${pdfName}` })
     return pdfName
 }
 
